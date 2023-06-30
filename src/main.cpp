c217c1629ed5214a29614825def837e75db41932
TaielST <tstecatto@gmail.com>
2023-06-30 11:42:12 -0300

    se esta agregando ultrasonido para probar

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,32 @@
 #define PIN_PWM_LEFT 22 // PWM
 #define PIN_DIR_RIGHT 18 // DIR
 #define PIN_PWM_RIGHT 19  // PWM
+#define PIN_ECHO_IZQ 25
+#define PIN_TRIG_IZQ 33
+#define PIN_ECHO_DER 27
+#define PIN_TRIG_DER 14
+
+#define DEBUG_ULTRASONIDO 500
+
+unsigned long currentTimeUltrasound = 0;
+
+float distaciaIzquierda;
+float distaciaDerecha;
 
 #define VELOCIDAD_ATAQUE_MAX 255
 #define VELOCIDAD_BUSQUEDA 80
 #define VELOCIDAD_ATAQUE_MIN 135
 #define VELOCIDAD_ATAQUE_MID 190
+
+Ultrasonido *ultrasonidoDerecho = new Ultrasonido(PIN_TRIG_DER, PIN_ECHO_DER);
+Ultrasonido *ultrsonidoIzquierdo = new Ultrasonido(PIN_TRIG_IZQ, PIN_ECHO_IZQ);
+
+
+void printUltra(){
+  if(millis() > currentTimeUltrasound + DEBUG_ULTRASONIDO){
+    
+  }
+}
 
 void adelante(){
   analogWrite(PIN_PWM_LEFT, VELOCIDAD_ATAQUE_MIN);
